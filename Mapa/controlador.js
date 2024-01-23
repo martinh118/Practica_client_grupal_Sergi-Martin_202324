a806beb242bd2f1c2542e99544861b236887711c
martinh118 <94359581+martinh118@users.noreply.github.com>
2024-01-23 12:33:25 +0100

    Actualización Mapa
    Muestra nombre comarcas al ser correctas.
    Muestra mensaje de incorrecto.
    Cambio de diseño.

--- Mapa/controlador.js
+++ Mapa/controlador.js
@@ -1,82 +1,145 @@
 let id_comarca;
 let div = "";
 let contador = 0;
+const svgNS = "http://www.w3.org/2000/svg";
 //dragenter
 //dragdiv
 //dragover
 //drop
 //dragstart
 
-document.getElementById("comarques").addEventListener("dragstart", function (event) {
+$("#comarques").on("dragstart", function (event) {
     id_comarca = event.target.id;
     div = document.getElementById(id_comarca);
-    
-    if(div.id != "comarques"){
+
+    if (div.id != "comarques") {
         console.log(event.target.id);
     }
-        
+
 });
 
 document.getElementById("comarques").addEventListener("dragend", function (event) {
-   
-        
+
+
 });
 
-document.getElementById("Catalunya").addEventListener("drop", function(event){
+
+
+$("#Catalunya").on("drop", function (event) {
     event.preventDefault();
     let zona = event.target.id;
     let divZona = document.getElementById(zona);
 
-    if(divZona.id != "Catalunya"){
+    if (divZona.id != "Catalunya") {
         console.log("Drop in: " + divZona.id);
         let comarc = div.id.replace("_", "");
         //console.log("comarc: "+comarc);
         if (divZona.id == comarc) {
+
+            var bbox = divZona.getBBox();
+            var x = (bbox.x + bbox.width / 2) - 25;
+            var y = (bbox.y + bbox.height / 2) + 15;
+
+/** 
+            let x = event.offsetX;
+            let y = event.offsetY;
+    */        
+            mostrarNombre(comarc, x, y);
+
             divZona.setAttribute("fill", "green");
             divZona.setAttribute("class", "correct");
             contador++;
             document.getElementById("contador").innerHTML = contador;
             div.remove();
             mostrarCongrats();
-        }else if(divZona.getAttribute("class") != "correct" && divZona.id != comarc){
+        } else if (divZona.getAttribute("class") != "correct" && divZona.id != comarc) {
             divZona.removeAttribute("fill");
-        }   
+            mostrarIncorrecte();
+        }
     }
 });
 
-document.getElementById("Catalunya").addEventListener("dragover", function(event){
+function mostrarNombre(comarca, x , y) {
+    let $prov = document.getElementById("Catalunya");
+    let t = document.createElementNS(svgNS, "text");
+    t.appendChild(document.createTextNode(comarca));
+    //console.log("x: " + x + " - y: " + y + " - comarca: "+comarca);
+    t.setAttribute("style", "position: absolute;font-size:20px");
+    t.setAttribute("x", x);
+    t.setAttribute("y", y);
+    $prov.appendChild(t);
+}
+
+$("#Catalunya").on("dragover", function (event) {
     event.preventDefault();
     let zona = event.target.id;
     let divZona = document.getElementById(zona);
 
-    if(divZona.id != "Catalunya"){
-        if(divZona.getAttribute("class") != "correct"){
+    if (divZona.id != "Catalunya") {
+        if (divZona.getAttribute("class") != "correct") {
             divZona.setAttribute("fill", "red");
         }
     }
 });
 
-document.getElementById("Catalunya").addEventListener("dragleave", function(event){
+$("#Catalunya").on("dragleave", function (event) {
     event.preventDefault();
     let zona = event.target.id;
     let divZona = document.getElementById(zona);
 
-    if(divZona.id != "Catalunya"){
-        if(divZona.getAttribute("class") != "correct"){
+    if (divZona.id != "Catalunya") {
+        if (divZona.getAttribute("class") != "correct") {
             divZona.removeAttribute("fill");
         }
-        
+
     }
 });
 
 
-function mostrarCongrats(){
+function mostrarCongrats() {
     let elemento = document.createElement("h1");
     elemento.textContent = "CORRECTE!!";
-    elemento.setAttribute("style", "text-align: center; color: white")
-    let divMapa = document.getElementById("mapa");
-    divMapa.insertBefore(elemento, divMapa.firstChild);
-    setTimeout(function(){
-        elemento.remove();
-    }, 5000);
+    elemento.setAttribute("style", "text-align: center; color: green");
+    elemento.setAttribute("id", "correcto");
+
+    if(document.getElementById("correcto")){
+        setTimeout(function () {
+            elemento.remove();
+        }, 5000);
+    }else{
+        
+        $("#titulo").append(elemento);
+        /** 
+        let divMapa = document.getElementById("mapa");
+        divMapa.insertBefore(elemento, divMapa.firstChild);
+        */
+        setTimeout(function () {
+            elemento.remove();
+        }, 5000);
+    }
+    
+}
+
+function mostrarIncorrecte() {
+    let elemento = document.createElement("h1");
+    elemento.textContent = "INCORRECTE!!";
+    elemento.setAttribute("style", "text-align: center; color: red");
+    elemento.setAttribute("id", "incorrecte");
+
+    if(document.getElementById("incorrecte")){
+        setTimeout(function () {
+            elemento.remove();
+        }, 3000);
+    }else{
+        
+        $("#titulo").append(elemento);
+        /** 
+        let divMapa = document.getElementById("mapa");
+        divMapa.insertBefore(elemento, divMapa.firstChild);
+        */
+        setTimeout(function () {
+            elemento.remove();
+        }, 3000);
+    }
+    
 }
